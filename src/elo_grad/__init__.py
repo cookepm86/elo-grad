98d4db8a4f119d72e9e5086710f4cebcaa036949
Michael Cooke <cookepm@tcd.ie>
2024-10-14 06:37:31 +0100

    feat: Add Optimizer
    Optimizer class takes model and can calculate update steps to
    weights.

--- src/elo_grad/__init__.py
+++ src/elo_grad/__init__.py
@@ -1,27 +1,83 @@
 import abc
-from typing import Tuple
+from collections import defaultdict
+from typing import Tuple, Optional, Dict
 
 import math
 
 
 class Model(abc.ABC):
-    # TODO: add update method
+
+    loss: str
+
+    def __init__(
+        self,
+        default_init_weight: float,
+        init_weights: Optional[Dict[str, Tuple[Optional[int], float]]],
+    ) -> None:
+        self.weights: Dict[str, Tuple[Optional[int], float]] = defaultdict(
+            lambda: (None, default_init_weight)
+        )
+        self.init_weights: Optional[Dict[str, Tuple[Optional[int], float]]] = init_weights
+        if self.init_weights is not None:
+            self.weights = self.weights | self.init_weights
 
     @abc.abstractmethod
-    def calculate_expected_result(self, *args) -> float:
+    def calculate_gradient(self, y: int, *args) -> float:
+        ...
+
+    @abc.abstractmethod
+    def calculate_expected_score(self, *args) -> float:
+        ...
+
+
+class Optimizer(abc.ABC):
+
+    def __init__(self, model: Model) -> None:
+        self.model: Model = model
+
+    @abc.abstractmethod
+    def calculate_update_step(self, y: int, entity_1: str, entity_2: str) -> Tuple[float, ...]:
         ...
 
 
 class LogisticRegression(Model):
 
-    def __init__(self, alpha: Tuple[float, ...], beta: float):
-        self.alpha: Tuple[float, ...] = alpha
+    loss: str = "log-loss"
+
+    def __init__(
+        self,
+        beta: float,
+        default_init_weight: float,
+        init_weights: Optional[Dict[str, Tuple[Optional[int], float]]],
+    ) -> None:
+        super().__init__(default_init_weight, init_weights)
         self.beta: float = beta
 
-    def calculate_expected_result(self, *args) -> float:
-        if len(args) != len(self.alpha):
-            raise ValueError("Length of args/values must match length of alpha/coefficients.")
+    def calculate_gradient(self, y: int, *args) -> float:
+        if y not in {0, 1}:
+            raise ValueError("Invalid result value %s", y)
+        y_pred: float = self.calculate_expected_score(*args)
+
+        return y - y_pred
+
+    def calculate_expected_score(self, *args) -> float:
         # I couldn't see any obvious speed-up from using NumPy/Numba data
         # structures but should revisit this.
-        exp = sum(k * v for k, v in zip(self.alpha, args))
-        return 1 / (1 + math.pow(10, -exp / (2 * self.beta)))
+        return 1 / (1 + math.pow(10, -sum(args) / (2 * self.beta)))
+
+
+class SGDOptimizer(Optimizer):
+
+    def __init__(self, model: Model, alpha: float) -> None:
+        super().__init__(model)
+        self.alpha: float = alpha
+
+    def calculate_update_step(self, y: int, entity_1: str, entity_2: str) -> Tuple[float, ...]:
+        grad: float = self.model.calculate_gradient(
+            y,
+            self.model.weights[entity_1][1],
+            -self.model.weights[entity_2][1],
+        )
+        step: float = self.alpha * grad
+
+        return step, -step
